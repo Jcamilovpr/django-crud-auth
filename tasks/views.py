3b345fca09282deb9646a71974475ec6c4743fe2
Camilo <camilovargas300@gmail.com>
2025-09-10 15:33:16 +1000

    Create Home and Signup pages with user registration
    - Added home.html and signup.html templates
    - Updated URLs for home and signup routes
    - Implemented user registration with password match validation
    - Added validation to prevent duplicate usernames
    
    A

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        else:
+            return HttpResponse('Password do not match')
